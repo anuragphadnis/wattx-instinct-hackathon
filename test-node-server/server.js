439784777dff0901dc8748d092b0c8466325f589
Anurag <phadnis.anurag@gmail.com>
2023-05-25 18:59:15 +0530

    Add node server to test contracts

--- test-node-server/server.js
+++ test-node-server/server.js
@@ -5,9 +5,17 @@ const app = express();
 const port = 3000;
 
 // sendFile will go here
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/index2.html'));
+app.get('/wattx', function(req, res) {
+  res.sendFile(path.join(__dirname, '/wattx.html'));
 });
+
+app.get('/exchange', function(req, res) {
+  res.sendFile(path.join(__dirname, '/TokenExchange.html'));
+})
+
+app.get('/inr', function(req, res) {
+  res.sendFile(path.join(__dirname, '/inr.html'));
+})
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
